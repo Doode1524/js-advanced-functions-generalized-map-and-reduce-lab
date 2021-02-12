c40de33ea5579822c3967351d7e255b6b43d0aa3
Doode1524 <Doode1524@gmail.com>
2021-02-12 17:04:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+function map(sourceArray, func) {
+    const newArray = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        newArray.push(func(sourceArray[i]))
+    }
+    return newArray
+}
+
+function reduce(sourceArray, func, startingPoint) {
+    let total
+    let i = 0
+
+    if (startingPoint) {
+        total = startingPoint
+    } else {
+        total = sourceArray[0]
+        i++ // to skip first element in loop
+    }
+
+    for (i; i < sourceArray.length; i++) {
+        total = func(sourceArray[i], total)
+
+        //counter = not += b/c going to operate on counter in the function, 
+        // + will happen in callback function
+    }
+
+    return total
+}
